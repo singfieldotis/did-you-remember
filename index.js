673c8e6c45bf337c8eda2ae2f1645f34ea90ebd8
Otis Singfield <singfieldotis@gmail.com>
2023-05-27 20:41:56 -0400

    Changed title, created module
    Created a module to import js functionality
    
    changed title of app

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+
+const appSettings = {
+    databaseURL: "https://playground-605ca-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+
+
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
