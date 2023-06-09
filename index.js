b10961539a6876df9f493576a96190a04dd95ca3
Otis Singfield <singfieldotis@gmail.com>
2023-06-09 00:04:15 -0400

    remove func from html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import {
   getDatabase,
-  ref,
-  push,
   onValue,
+  push,
+  ref,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
